6846107c4877ba310872271b43add3e1a9d57462
TKC <r.tkr.tkc@gmail.com>
2025-09-29 22:40:23 +0900

    読み上げ速度を変更できるように修正

--- app/dashboard/components/ServerInfo.tsx
+++ app/dashboard/components/ServerInfo.tsx
@@ -28,6 +28,7 @@ export default function ServerInfo({ config }: any) {
       ...prev,
       [key]: newValue,
     }));
+    console.log(localConfig);
   };
 
 
@@ -37,6 +38,8 @@ export default function ServerInfo({ config }: any) {
       <h2 className="text-lg font-bold mb-4">サーバー設定</h2>
       <h3 className="mt-4 font-semibold">最大テキスト長: <input
         id="int"
+        type="number"
+        step="10"
         inputMode="numeric"
         pattern="\d*"
         value={localConfig.maxTextLength}
@@ -45,12 +48,22 @@ export default function ServerInfo({ config }: any) {
         placeholder="例: 12345"
         aria-label="整数入力"
       /></h3>
-      <h3 className="mt-4 font-semibold">デフォルト話者:</h3>
+      <h3 className="mt-4 font-semibold">デフォルト話者:
       <VvEngineSpeakers
         defaultId={localConfig["defaultSpeakerId"]}
         onChange={(id) => updateRootConfig("defaultSpeakerId", Number(id))}
-      />
-      <h3 className="mt-4 font-semibold">読み上げ速度: {localConfig.playbackSpeed}</h3>
+      /></h3>
+      <h3 className="mt-4 font-semibold">読み上げ速度: <input
+        id="float"
+        type="decimal"
+        step="0.1"
+        inputMode="numeric"
+        value={localConfig.playbackSpeed}
+        onChange={(e) => updateRootConfig("playbackSpeed", parseFloat(e.target.value))}
+        className="border p-1"
+        placeholder="例: 123.45"
+        aria-label="小数入力"
+      /></h3>
       <h3 className="mt-4 font-semibold">読み上げチャンネル一覧:</h3>
       <ul>
         {Object.entries(localConfig.talkChannel).map(([id, ch]) => {
